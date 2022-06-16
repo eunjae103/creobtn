412e1f5bce063b2592d94d48310735bee86406c8
eunjae <zhalr103@naver.com>
2022-06-16 15:38:40 +0900

    title추가 및 레시피 메모 합치기

--- src/ButtonsWrap/Buttons.js
+++ src/ButtonsWrap/Buttons.js
@@ -7,50 +7,63 @@ const Buttons = () => {
 
   const onClickChange = (e) => {
     setChangeButtons(Number(e.target.value));
-    console.log(e.target.value);
   };
 
   const [leftButtons, setLeftButtons] = useState([
-    { id: 1, text: "끔" },
-    { id: 2, text: "약불" },
-    { id: 3, text: "중불" },
-    { id: 4, text: "강불" },
+    { id: 1, title: "인덕션왼쪽", value: "끔" },
+    { id: 2, title: "인덕션왼쪽", value: "약불" },
+    { id: 3, title: "인덕션왼쪽", value: "중불" },
+    { id: 4, title: "인덕션왼쪽", value: "강불" },
   ]);
 
   const [potAngleButtons, setPotAngleButtons] = useState([
-    { id: 1, text: "재료담기" },
-    { id: 2, text: "면요리" },
-    { id: 3, text: "기본요리" },
-    { id: 4, text: "음식담기" },
-    { id: 5, text: "세척준비" },
+    { id: 1, title: "팟각도", value: "재료담기" },
+    { id: 2, title: "팟각도", value: "면요리" },
+    { id: 3, title: "팟각도", value: "기본요리" },
+    { id: 4, title: "팟각도", value: "음식담기" },
+    { id: 5, title: "팟각도", value: "세척준비" },
   ]);
 
   const [potSpinButtons, setSpinButtons] = useState([
-    { id: 1, text: "역회전" },
-    { id: 2, text: "정지" },
-    { id: 3, text: "원점정지" },
-    { id: 4, text: "정회전" },
+    { id: 1, title: "팟회전", value: "역회전" },
+    { id: 2, title: "팟회전", value: "정지" },
+    { id: 3, title: "팟회전", value: "원점정지" },
+    { id: 4, title: "팟회전", value: "정회전" },
   ]);
 
   const [cleanButtons, setCleanButtons] = useState([
-    { id: 1, text: "세척중" },
-    { id: 2, text: "정지" },
+    { id: 1, title: "세척", value: "세척중" },
+    { id: 2, title: "세척", value: "정지" },
   ]);
 
   const [rightButtons, setRightButtons] = useState([
-    { id: 1, text: "끔" },
-    { id: 2, text: "약불" },
-    { id: 3, text: "중불" },
-    { id: 4, text: "강불" },
+    { id: 1, title: "인덕션오른쪽", value: "끔" },
+    { id: 2, title: "인덕션오른쪽", value: "약불" },
+    { id: 3, title: "인덕션오른쪽", value: "중불" },
+    { id: 4, title: "인덕션오른쪽", value: "강불" },
   ]);
-  const [recipeName, setRecipeName] = useState("");
-  const [recipeMemo, setRecipeMemo] = useState("");
-  const onChangeName = (e) => {
-    setRecipeName(e.target.value);
+
+  const [recipes, setRecipes] = useState({
+    recipeName: "",
+    recipeMemo: "",
+  });
+  const { recipeName, recipeMemo } = recipes;
+  const onChange = (e) => {
+    const { value, name } = e.target;
+    setRecipes({
+      ...recipes,
+      [name]: value,
+    });
   };
-  const onChangeMemo = (e) => {
-    setRecipeMemo(e.target.value);
+  const [lapTime, setLapTime] = useState({ name: "", value: "" });
+  const { name, value } = lapTime;
+  const onClickSave = (e) => {
+    const { value, name } = e.target;
+    setLapTime({ ...lapTime, [name]: value });
+    // setLapTime(e.target.attributes.title.textContent);
+    console.log(e.target);
   };
+
   return (
     <>
       <SelectBtnWrap>
@@ -73,8 +86,14 @@ const Buttons = () => {
           <ul>
             {leftButtons.map((b) => {
               return (
-                <li key={b.id} value={b.text}>
-                  {b.text}
+                <li
+                  name="인덕션왼쪽"
+                  key={b.id}
+                  title={b.title}
+                  value={b.value}
+                  onClick={onClickSave}
+                >
+                  {b.value}
                 </li>
               );
             })}
@@ -87,8 +106,8 @@ const Buttons = () => {
             <ul>
               {potAngleButtons.map((b) => {
                 return (
-                  <li key={b.id} value={b.text}>
-                    {b.text}
+                  <li key={b.id} value={b.value} onClick={onClickSave}>
+                    {b.value}
                   </li>
                 );
               })}
@@ -99,8 +118,8 @@ const Buttons = () => {
             <ul>
               {potSpinButtons.map((b) => {
                 return (
-                  <li key={b.id} value={b.text}>
-                    {b.text}
+                  <li key={b.id} value={b.value} onClick={onClickSave}>
+                    {b.value}
                   </li>
                 );
               })}
@@ -111,8 +130,8 @@ const Buttons = () => {
             <ul>
               {cleanButtons.map((b) => {
                 return (
-                  <li key={b.id} value={b.text}>
-                    {b.text}
+                  <li key={b.id} value={b.value} onClick={onClickSave}>
+                    {b.value}
                   </li>
                 );
               })}
@@ -124,8 +143,8 @@ const Buttons = () => {
           <ul>
             {rightButtons.map((b) => {
               return (
-                <li key={b.id} value={b.text}>
-                  {b.text}
+                <li key={b.id} value={b.value} onClick={onClickSave}>
+                  {b.value}
                 </li>
               );
             })}
@@ -134,20 +153,24 @@ const Buttons = () => {
       </InductionWrap>
       <Memowrap>
         <input
-          type="text"
+          name="recipeName"
           value={recipeName}
-          onChange={onChangeName}
+          onChange={onChange}
           placeholder="레시피 이름을 입력해주세요."
         ></input>
         <textarea
+          name="recipeMemo"
           value={recipeMemo}
-          onChange={onChangeMemo}
+          onChange={onChange}
           placeholder="메모를 입력해주세요."
         ></textarea>
         <div>
           <p>레시피 : {recipeName}</p>
           <p>메모 : {recipeMemo}</p>
-          <p>기기작동기록 : </p>
+          <p>
+            기기작동기록 : {name}
+            {value}
+          </p>
         </div>
       </Memowrap>
     </>
