8a2de38fbbc5dc16bd5c99771281276b12f484c4
eunjae <zhalr103@naver.com>
2022-06-28 13:37:53 +0900

    리팩토링

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import ButtonsWrap from "./ButtonsWrap/ButtonsWrap";
 import styled from "styled-components";
-import Time from "./ButtonsWrap/Time";
-import Timer from "./ButtonsWrap/Timer";
-import Stopwatch from "./ButtonsWrap/Stopwatch";
 
 function App() {
   return <ButtonsWrap />;
